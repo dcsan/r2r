7800c8cb3f68f3eda8ec236ff3348ee4e94b1452
Ayinde Williams <graywolfstudios@gmail.com>
2015-02-07 16:30:06 -0800

    initial android push

---
+++ app/src/main/java/com/darkbot/litterator/app/MainActivity.java
@@ -0,0 +1,53 @@
+package com.darkbot.litterator.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView litWebView  = (WebView)findViewById(R.id.litWebView);
+
+        litWebView.loadUrl("http://r2r.meteor.com");
+        litWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        litWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = litWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
