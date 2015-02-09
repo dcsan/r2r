57a988a3cb8ae44948067f0edb7e918301403994
Ayinde Williams <graywolfstudios@gmail.com>
2015-02-08 16:43:06 -0800

    Allow back key to go back in web history

--- app/src/main/java/com/darkbot/litterator/app/MainActivity.java
+++ app/src/main/java/com/darkbot/litterator/app/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -63,6 +64,17 @@ public class MainActivity extends Activity {
         });
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && litWebView.canGoBack()) {
+            litWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onPause() {
